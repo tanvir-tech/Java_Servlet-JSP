3bf8e0e32123e65d82457385a997f28cc394ad5a
tanvir-tech <tanvirah325@gmail.com>
2021-02-23 09:56:06 +0600

    requestDispatcher -> to call servlet from servlet

--- src/main/java/controller/AddServlet.java
+++ src/main/java/controller/AddServlet.java
@@ -1,20 +1,24 @@
 package main.java.controller;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class Add_Servlet extends HttpServlet {
+public class AddServlet extends HttpServlet {
 
-    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
 
         int sum = num1+num2;
-        System.out.println("Sum is ="+sum);
+
+        req.setAttribute("sum",sum);
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("result");
+        requestDispatcher.forward(req,res);
     }
 }
---
+++ src/main/java/controller/ResultServlet.java
@@ -0,0 +1,21 @@
+package main.java.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ResultServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        int sum = (int)req.getAttribute("sum");
+
+        PrintWriter out = res.getWriter();
+        out.println("Sum is = "+sum);
+
+    }
+}
